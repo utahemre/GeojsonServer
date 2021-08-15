aa9b528071662b026aed42b84ccab7802fe1a0bc
Yunus Emre Özkaya <yeozkaya@gmail.com>
2021-08-15 11:58:59 +0300

    Update index.js
    Add remove layers programmatically

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,25 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 var map;
 
 window.onload = function (e) {
 
-
-    mapboxgl.accessToken = 'YOUR_ACCESS_TOKEN';
+    mapboxgl.accessToken = 'pk.eyJ1IjoidXRhaGVtcmUiLCJhIjoiY2lmM3RxcWp6MDBtM3RsbHlvZTRxd2lvaiJ9._01IsYjztRQ0DhF_lt5y2A';
     map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        style: 'mapbox://styles/mapbox/outdoors-v11', // stylesheet location
         center: [35, 40], // starting position [lng, lat]
         zoom: 5 // starting zoom
     });
-
-    //addPointLayerToMap();
-    //addLineLayerToMap();
-    //addPolygonLayerToMap();
-    addPopulationPolygonLayerToMap();
+    
 };
 
-
 function getGeojson(query, _callback) {
-    url = "/geojsonserver/geojson/getByQuery?query=" + query;
+    var url = "/geojsonserver/geojson/getByQuery?query=" + query;
 
     fetch(url).then(res => res.json())
             .then((res) => {
@@ -47,8 +47,8 @@ function addPointLayerToMap() {
                     'type': 'circle',
                     'source': "pointSourceId",
                     'paint': {
-                        'circle-radius': 5,
-                        'circle-color': '#0000FF'
+                        'circle-radius': 3,
+                        'circle-color': '#FF00FF'
                     },
                     'minZoom': 5,
                     'maxZoom': 15
@@ -59,7 +59,7 @@ function addPointLayerToMap() {
         
         var popup;
         
-        map.on('mouseenter', 'pointLayerId', function (e) {
+        map.on('mousemove', 'pointLayerId', function (e) {
             popup = new mapboxgl.Popup({closeButton: false})
                     .setLngLat(e.lngLat)
                     .setHTML(e.features[0].properties.tipi)
@@ -70,8 +70,7 @@ function addPointLayerToMap() {
             popup.remove();
         });
     });
-}
-;
+};
 
 function addLineLayerToMap() {
 
@@ -88,7 +87,7 @@ function addLineLayerToMap() {
                     'type': 'line',
                     'source': "lineSourceId",
                     'paint': {
-                        'line-width': 3,
+                        'line-width': 5,
                         'line-color': '#FF0000'
                     },
                     'minZoom': 5,
@@ -99,7 +98,7 @@ function addLineLayerToMap() {
         this.map.addLayer(lineLayer);
         
         
-        map.on('mouseenter', 'lineLayerId', function (e) {
+        map.on('mousemove', 'lineLayerId', function (e) {
             popup = new mapboxgl.Popup({closeButton: false})
                     .setLngLat(e.lngLat)
                     .setHTML(e.features[0].properties.adi)
@@ -116,7 +115,7 @@ function addLineLayerToMap() {
 
 function addPolygonLayerToMap() {
 
-    getGeojson("select * from egitim_poligon where il='Ankara'", (_result) => {
+    getGeojson("select * from egitim_poligon", (_result) => {
 
         map.addSource("polygonSourceId", {
             'type': 'geojson',
@@ -130,7 +129,8 @@ function addPolygonLayerToMap() {
                     'source': "polygonSourceId",
                     'paint': {
                         'fill-opacity': 0.3,
-                        'fill-color': '#00FF00',
+                        'fill-color': '#FF00FF',
+                        'fill-outline-color': '#000000'
 
                     },
                     'minZoom': 5,
@@ -153,16 +153,16 @@ function addPopulationPolygonLayerToMap() {
 
     getGeojson("select * from egitim_poligon", (_result) => {
 
-        map.addSource("polygonSourceId", {
+        map.addSource("populationPolygonSourceId", {
             'type': 'geojson',
             'data': _result
         });
 
         var polygonLayer =
                 {
-                    'id': "polygonLayerId",
+                    'id': "populationPolygonLayerId",
                     'type': 'fill',
-                    'source': "polygonSourceId",
+                    'source': "populationPolygonSourceId",
                     'paint': {
                         'fill-opacity': 0.5,
                         'fill-color':
@@ -188,11 +188,102 @@ function addPopulationPolygonLayerToMap() {
 
         this.map.addLayer(polygonLayer);
 
-        map.on('click', 'polygonLayerId', function (e) {
+        map.on('click', 'populationPolygonLayerId', function (e) {
             new mapboxgl.Popup()
                     .setLngLat(e.lngLat)
                     .setHTML(e.features[0].properties.il + " : " + e.features[0].properties.nufus)
                     .addTo(map);
         });
     });
+}
+
+function addPopulationPolygon3DLayerToMap() {
+
+    getGeojson("select * from egitim_poligon", (_result) => {
+
+        map.addSource("populationPolygon3DSourceId", {
+            'type': 'geojson',
+            'data': _result
+        });
+
+        var polygonLayer =
+                {
+                    'id': "populationPolygon3DLayerId",
+                    'type': 'fill-extrusion',
+                    'source': "populationPolygon3DSourceId",
+                    'paint': {
+                        'fill-extrusion-opacity': 0.5,
+                        'fill-extrusion-color':
+                                [
+                                    'step',
+                                    ['get', 'nufus'],
+                                    '#00FF00',
+                                    500000,
+                                    '#FFFF00',
+                                    1000000,
+                                    '#FFA500',
+                                    3000000,
+                                    '#FF0000',
+                                    5000000,
+                                    '#8B0000'
+                                ],
+                        'fill-extrusion-height':
+                                [
+                                    'step',
+                                    ['get', 'nufus'],
+                                    5000,
+                                    500000,
+                                    10000,
+                                    1000000,
+                                    20000,
+                                    3000000,
+                                    40000,
+                                    5000000,
+                                    80000
+                                ],        
+
+                    },
+                    'minZoom': 5,
+                    'maxZoom': 15
+
+                };
+
+        this.map.addLayer(polygonLayer);
+
+        map.on('click', 'populationPolygonLayerId', function (e) {
+            new mapboxgl.Popup()
+                    .setLngLat(e.lngLat)
+                    .setHTML(e.features[0].properties.il + " : " + e.features[0].properties.nufus)
+                    .addTo(map);
+        });
+    });
+}
+
+function removePointLayerFromMap() {
+    map.removeLayer("pointLayerId");
+    map.removeSource("pointSourceId");
+}
+
+function removeLineLayerFromMap() {
+    map.removeLayer("lineLayerId");
+    map.removeSource("lineSourceId");
+}
+
+function removePolygonLayerFromMap() {
+    map.removeLayer("polygonLayerId");
+    map.removeSource("polygonSourceId");
+}
+
+function removePopulationPolygonLayerFromMap() {
+    map.removeLayer("populationPolygonLayerId");
+    map.removeSource("populationPolygonSourceId");
+}
+
+function removePopulationPolygon3DLayerFromMap() {
+    map.removeLayer("populationPolygon3DLayerId");
+    map.removeSource("populationPolygon3DSourceId");
+}
+
+function changeStyle(layerId) {
+    map.setStyle('mapbox://styles/mapbox/' + layerId);
 }
